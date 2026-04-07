b97d7122ce5642229c70f8b3a30b939faa6d0cd1
sihamkassim <sihamkassim735@gmail.com>
2026-04-07 06:51:12 +0300

    calculator

---
+++ level3_calculator.py
@@ -0,0 +1,36 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+
+def calculator():
+    print("Simple CLI Calculator")
+    print("Operations: +  -  *  /")
+
+    a = float(input("Enter first number: "))
+    op = input("Enter operation: ")
+    b = float(input("Enter second number: "))
+
+    if op == '+':
+        print("Result:", add(a, b))
+    elif op == '-':
+        print("Result:", subtract(a, b))
+    elif op == '*':
+        print("Result:", multiply(a, b))
+    elif op == '/':
+        print("Result:", divide(a, b))
+    else:
+        print("Invalid operation")
+
+
+calculator()
