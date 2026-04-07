387e00a32e673907d9b374d96e5e9450a670df7c
sihamkassim <sihamkassim735@gmail.com>
2026-04-07 06:50:49 +0300

    Add fibonacci

---
+++ level2_fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    """Generate Fibonacci sequence up to n terms."""
+    sequence = []
+    a, b = 0, 1
+
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+
+    return sequence
+
+
+terms = int(input("Enter number of terms: "))
+print("Fibonacci sequence:", fibonacci(terms))
